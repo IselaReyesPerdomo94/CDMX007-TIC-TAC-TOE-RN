58c6f4174edaf01d3e36954213452274c69f9456
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-10-27 17:15:40 -0600

    ADD: Adding babel, react and webpack and their configuration. THis closes #2

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const root = path.resolve(__dirname, 'src')
+
+const config = {
+    entry: {
+        main: `${root}/index.jsx`
+    },
+    output: {
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        port: 8080,
+        contentBase: path.resolve(__dirname, 'public'),
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            views: `${root}/views`,
+            components: `${root}/components`
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.js(x)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: './index.ejs',
+            title: 'Tic tac toe',
+            filename: './index.html'
+        })
+    ]
+}
+
+module.exports = config;
