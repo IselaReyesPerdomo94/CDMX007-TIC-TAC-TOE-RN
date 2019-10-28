538e017df4c2e2408275b64b2c085972e3f45425
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-10-27 22:48:15 -0600

    HOME: Adding home component. Changing styles and installing css loader and style loader. This closes #12

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Home from 'views/Home/';
+import Game from 'views/Game/';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const App = () => <div>App from react</div>
+const App = () => {
+    return(
+        <Router  basename={window.location.pathname || ''}>
+            <Switch>
+                <Route exact path="/" ><Home/></Route>
+                <Route exact path="/Game"><Game/></Route>
+            </Switch>
+        </Router>
+    )
+}
+
 
 const container = document.querySelector('#root');
 
---
+++ src/views/Game/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Game extends Component{
+    render(){
+        return(
+            <h4>Michi</h4>
+        )
+    }
+}
+
+export default Game;
