675a6277e318860fbb77192b875ab4c9263b37e3
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-10-28 17:31:57 -0600

    Adding board component and styling

---
+++ src/components/Squarebox/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+const SquareBox = () => {
+    return(
+        <button className="square-box"></button>
+    )
+}
+
+export default SquareBox;
--- src/views/Game/index.jsx
+++ src/views/Game/index.jsx
@@ -1,9 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import Board from 'components/Board';
+import './index.css';
 
 class Game extends Component{
     render(){
         return(
-            <h4>Michi</h4>
+            <Fragment>
+                <header>
+                <h4 className="title">Es turno de </h4><span></span>
+                </header>
+                <main className="game">
+                    <Board/>
+                </main>
+            </Fragment>
         )
     }
 }
