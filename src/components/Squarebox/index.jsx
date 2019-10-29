1ff5a1ee1330381b5ce46b92d3acd4f616bf8fd4
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-10-29 15:41:17 -0600

    ADD: Adding function to show player in course and displaying x or o acording to the player

--- src/components/Squarebox/index.jsx
+++ src/components/Squarebox/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './index.css';
 
-const SquareBox = () => {
-    return(
-        <button className="square-box"></button>
-    )
+class SquareBox extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+
+        this.printAndChangeTurn = this.printAndChangeTurn.bind(this)
+    }
+    printAndChangeTurn(){
+        const {turn, changeTurn} = this.props;
+        const symbol = `${turn ? 'X': 'O'}`
+        this.setState({value: symbol})
+        changeTurn()
+    }
+   render(){
+       
+       return( 
+           <button className="square-box" onClick={()=> this.printAndChangeTurn()}>{this.state.value}</button>
+       )
+   }
 }
 
 export default SquareBox;
